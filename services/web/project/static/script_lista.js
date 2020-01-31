5daf4198448a02998c21090e2be5f817c5f661ca
cris-Duarte <crisjavierduarte@gmail.com>
2020-01-30 23:19:35 -0300

    cambio de nombre de clase para elementos del NodeList. Mensaje sobre fin de lista

--- services/web/project/static/script_lista.js
+++ services/web/project/static/script_lista.js
@@ -11,6 +11,7 @@ document.addEventListener('DOMContentLoaded', () => {
   document.getElementById('actual').appendChild(duplalumno);
   duplalumno = alumnos[1].cloneNode(true);
   duplalumno.style.display = 'block';
+  duplalumno.style.opacity = 0.2;
   document.getElementById('siguiente').appendChild(duplalumno);
   alert('ok');
 });
@@ -22,10 +23,11 @@ var comenzar = function () {
 }
 
 var siguiente = function () {
-  if (indice_actual < alumnos.length - 1) {
+  if (indice_actual < alumnos.length) {
     indice_actual++;
     duplalumno = alumnos[indice_actual-1].cloneNode(true);
     duplalumno.style.display = 'block';
+    duplalumno.style.opacity = 0.2;
     p = document.getElementById('anterior');
     if (indice_actual == 1) {
       p.appendChild(duplalumno);
@@ -33,16 +35,23 @@ var siguiente = function () {
       p.replaceChild(duplalumno, p.childNodes[0]);
     }
 
+    if (indice_actual == alumnos.length) {
+      p = document.getElementById('actual');
+      p.innerHTML = '<p class="alumno label label-info label-alumno">Ya haz llamado toda la lista</p>';
+    } else {
     duplalumno = alumnos[indice_actual].cloneNode(true);
     duplalumno.style.display = 'block';
     p = document.getElementById('actual');
     p.replaceChild(duplalumno, p.childNodes[0]);
-    if (indice_actual == alumnos.length - 1) {
+    }
+    if (indice_actual == alumnos.length) {
       p = document.getElementById('siguiente');
       p.removeChild(p.childNodes[0]);
-    } else {
+    }
+    else {
       duplalumno = alumnos[indice_actual+1].cloneNode(true);
       duplalumno.style.display = 'block';
+      duplalumno.style.opacity = 0.2;
       p = document.getElementById('siguiente');
       p.replaceChild(duplalumno, p.childNodes[0]);
   }
@@ -59,6 +68,7 @@ var atras = function () {
     } else {
     duplalumno = alumnos[indice_actual-1].cloneNode(true);
     duplalumno.style.display = 'block';
+    duplalumno.style.opacity = 0.2;
     p = document.getElementById('anterior');
     p.replaceChild(duplalumno, p.childNodes[0]);
   }
@@ -69,8 +79,9 @@ var atras = function () {
 
     duplalumno = alumnos[indice_actual+1].cloneNode(true);
     duplalumno.style.display = 'block';
+    duplalumno.style.opacity = 0.2;
     p = document.getElementById('siguiente');
-    if (indice_actual == alumnos.length-2) {
+    if (indice_actual == alumnos.length) {
       p.appendChild(duplalumno);
     } else {
       p.replaceChild(duplalumno, p.childNodes[0]);
@@ -79,9 +90,11 @@ var atras = function () {
 };
 
 var presente = function () {
+  alumnos[indice_actual].className = 'alumno label label-success label-alumno'
   siguiente();
 }
 
 var ausente = function () {
+  alumnos[indice_actual].className = 'alumno label label-warning label-alumno'
   siguiente();
 }
