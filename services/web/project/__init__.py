694135eb648dc393d7004dc700f49cf7640aa718
cris-Duarte <crisjavierduarte@gmail.com>
2020-02-24 12:57:16 -0300

    .

--- services/web/project/__init__.py
+++ services/web/project/__init__.py
@@ -30,7 +30,7 @@ GOOGLE_DISCOVERY_URL = ("https://accounts.google.com/.well-known/openid-configur
 @app.route("/gLogin")
 def gLogin():
     if current_user.is_active:
-        return redirect(url_for('/dashboard'))
+        return redirect(url_for('dashboard'))
     else:
         return render_template("login.html",g=GOOGLE_CLIENT_ID)
 
@@ -47,7 +47,7 @@ def create_or_update_user():
         u = load_user(user.id)
         login_user(user,remember=True, force=False, fresh=True)
 
-        return redirect(url_for('/dashboard'))
+        return redirect(url_for('dashboard'))
     else:
         return 'Hubo un problema con la autenticacion'+mensaje
 
@@ -73,12 +73,12 @@ def login():
     else:
         u = load_user(u.id)
         login_user(u,remember=True, force=False, fresh=True)
-        return redirect(url_for('/dashboard'), code=303)
+        return redirect(url_for('dashboard'), code=303)
 
 @app.route("/salir", methods=['POST'])
 def salir():
     logout_user()
-    return redirect(url_for('/ingresar'))
+    return redirect(url_for('ingresar'))
 
 ## FIN LOGIN SYSTEM
 
